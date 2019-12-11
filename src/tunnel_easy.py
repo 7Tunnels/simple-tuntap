22b7d3d2aefb77dcd183e44363962771b3546a2c
KodyKendall <Kodyckendall@gmail.com>
2019-12-10 17:28:33 -0700

    Figured out my original issue with the src and dst fields being messed up

--- src/tunnel_easy.py
+++ src/tunnel_easy.py
@@ -4,9 +4,11 @@ import logging
 import socket
 import tunnel
 import pdb
+import binascii
 from scapy.layers.inet import IP, ICMP
 from scapy.sendrecv import sr
 from scapy.all import send 
+import sys
 
 LOGLEVEL = logging.DEBUG
 LOGFORMAT = '%(name)s - %(levelname)s - %(message)s'
@@ -23,25 +25,34 @@ if __name__ == '__main__':
         """Echo the received data back to the sender
         but swap source and dest IPs"""
         # Assume it's L3
-        #print("Here is the data received..: {}".format(data))
-        #TODO: Here is where we would print the contents of the packet using scapy.
-        print(IP(data).show())
-        send(IP(data))
-
-#        IPPacket = Packet(pkt)a
-        #TODO: Here is where we would forward the packet using scapy.
-
-        #TODO: Then, we would take the packet and write it back to the tun..
-
         leading_bytes = data[:4]
         data = data[4:]  # Strip off the first 4 bytes
-        ip_pkt = dpkt.ip.IP(data)
-        src = ip_pkt.src
-        dst = ip_pkt.dst
-        ip_pkt.src = dst
-        ip_pkt.dst = src
-        data = leading_bytes + ip_pkt.__bytes__()
-        t.send(data)
+
+        if IP(data).src == "0.0.0.0":
+            return #Don't handle broadcasted packets.
+        print "Sender: {0}\nReceiver: {1}".format(IP(data).src, IP(data).dst)
+        # print "Raw Data as hex: \n"
+        # print data.encode('hex')
+        # print "\n"
+        print IP(data).show()        
+        # p = sr(IP(data))
+
+#        IPPacket = Packet(pkt)
+        #TODO: Here is where we would forward the packet using scapy.
+        #TODO: Then, we would take the packet and write it back to the tun..
+
+        # try:
+        #     ip_pkt = dpkt.ip.IP(data)
+        # except: 
+        #     print("Error: {}".format(sys.exc_info()[0]))
+        #     return
+        # src = ip_pkt.src
+        # dst = ip_pkt.dst
+        # ip_pkt.src = dst
+        # ip_pkt.dst = src
+        # data = leading_bytes + ip_pkt.__bytes__()
+
+        t.send(p)
     t.set_rx_handler(echohandler)
     t.monitor()
 
@@ -49,5 +60,3 @@ if __name__ == '__main__':
         raw_input('Wait indefinitely. Press ctrl-c to quit.')
     except KeyboardInterrupt:
         t.close()
-
-
